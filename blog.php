47698b0cbd6a18e61291b0d9319dee25ba363a56
Harshal Sawatkar <72376618+Anonymous-hss@users.noreply.github.com>
2023-03-28 09:05:07 +0530

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,181 @@
+<?php
+session_start();
+
+if(isset($_SESSION['login'])) {
+    $fname = $_SESSION['fname'];
+    $lname = $_SESSION['lname'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>The Fresh Cup - Blog</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/business-casual.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="brand">The Fresh Cup</div>
+    <div class="address-bar">3481 Melrose Place | Beverly Hills, CA 90210 | 123.456.7890</div>
+
+    <!-- Navigation -->
+    <?php require_once 'nav.php'; ?>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <h2 class = "text-center">Welcome <?php echo $fname; echo " "; echo $lname; ?> - <a href="logout.php">Logout</a></h2>
+                    <hr>
+                    <h2 class="intro-text text-center">
+                        <strong>The Fresh Cup</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <img class="img-responsive img-border img-full" src="img/slide-1.jpg" alt="">
+                    <h2>Espresso
+                        <br>
+                        <small>October 13, 2022</small>
+                    </h2>
+                    <p> A concentrated shot of coffee made by forcing pressurized hot water through finely ground coffee beans.</p>
+                    <button type ="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Read More</button>
+                    <hr>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <img class="img-responsive img-border img-full" src="img/slide-2.jpg" alt="">
+                    <h2>French Press
+                        <br>
+                        <small>November 18, 2022</small>
+                    </h2>
+                    <p>Coffee made by steeping medium-coarse coffee grounds in hot water in a French press, then pressing down a plunger to separate the coffee from the grounds.</p>
+                    <button type ="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal1">Read More</button>
+
+                    <hr>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <img class="img-responsive img-border img-full" src="img/slide-3.jpg" alt="">
+                    <h2>Cold Brew
+                        <br>
+                        <small>December 23, 2022</small>
+                    </h2>
+                    <p>Coffee made by steeping coffee grounds in cold water for an extended period of time, resulting in a smooth, less acidic coffee.</p>
+                    <button type ="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal2">Read More</button>
+                    <hr>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <ul class="pager">
+                        <li class="previous"><a href="#">&larr; Older</a>
+                        </li>
+                        <li class="next"><a href="#">Newer &rarr;</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="clone" data-dismiss="model">&times;</button>
+                    <h4 class="model-title">Espresso</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Sample Text.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="myModal1" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="clone" data-dismiss="model">&times;</button>
+                    <h4 class="model-title">Frech Press</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Sample Text.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div id="myModal2" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="clone" data-dismiss="model">&times;</button>
+                    <h4 class="model-title">COLD BREW</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Sample Text.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; The Fresh Cup 2023</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
+
+<?php
+} else {
+    header("location:login.php");
+}
+?>
